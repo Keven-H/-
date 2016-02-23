05b8415f26bd352ecd0a44544586fe10e70ed41d
JacyFox <JacyFox@foxmail.com>
2016-02-23 11:47:16 +0800

    Initial Commit

---
+++ 图片浏览器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片浏览器
+//
+//  Created by Admin on 16/2/23.
+//  Copyright © 2016年 MYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图片浏览器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片浏览器
+//
+//  Created by Admin on 16/2/23.
+//  Copyright © 2016年 MYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
